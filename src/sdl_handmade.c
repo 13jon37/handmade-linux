0db080de778a6ebaa442c376082d4ef9e8bddcd4
13jon37 <jonlockllyr2@gmail.com>
2020-11-25 15:57:47 -0500

    e

--- src/sdl_handmade.c
+++ src/sdl_handmade.c
@@ -21,9 +21,6 @@ global_variable int bytes_per_pixel = 4;
 internal void
 draw_filled_rect(v4 color, v2 position, v2 size)
 {
-    // Credit = Ryan Feury 
-    // A great help
-    
     i32 lower_bound_x = (i32)position.x;
     i32 lower_bound_y = (i32)position.y;
     i32 upper_bound_x = lower_bound_x + (i32)size.x;
